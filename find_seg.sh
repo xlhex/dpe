4800d66e407c4c25ff106d7a1784641d89ce8c7c
Zodiac He <zodiachy@gmail.com>
2020-10-26 23:41:20 +1100

    fix bugs in find_seg.sh

--- find_seg.sh
+++ find_seg.sh
@@ -11,4 +11,4 @@ CKPT=$MPATH/checkpoint_best.pt
 TASK=translation
 BATCH=35
 OUTPUT=$MPATH/$subset.$SRC-$TGT.$TGT
-python3 ./find_seg.py $DATA --task $TASK --segment --raw-text -s $SRC -t $TGT --path $CKPT --batch-size $BATCH --beam $BEAM --max-len-b 100 --gen-subset $subset > $OUTPUT
+python3 ./find_seg.py $DATA --task $TASK --segment --raw-text -s $SRC -t $TGT --path $CKPT --batch-size $BATCH --max-len-b 100 --gen-subset $subset > $OUTPUT
